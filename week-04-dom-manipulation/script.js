18440017efaf0e34bd32d223592f3bdc460300ec
elparker13 <elparker@millersville.edu>
2022-02-16 18:05:20 -0500

    script to body

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -6,6 +6,6 @@ let verb = prompt('Enter an action verb not ending in ING:');
 let adjective = prompt('Enter an adjective:');
 let pluralNoun = prompt('Enter a plural noun:');
 
-document.write(yourName + ' just got offered an new job as a ' + occupation
-+ ' making ' + salary + ' thousand dollars per week. ' + yourExp + ' you say! Your new job
-requires you to ' + verb + ' and make ' + adjective + ' ' + pluralNoun + ' . ');
+document.write(yourName + " just got offered an new job as a " + occupation
++ " making " + salary + " thousand dollars per week. " + yourExp + " you say."
++ "Your new job requires you to " + verb + " and make " + adjective + " " + pluralNoun + " . ");
