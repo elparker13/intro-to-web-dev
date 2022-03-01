7e2f6375fac9d1bceb6ccb1c73cb8f91b7697b59
elparker13 <elparker@millersville.edu>
2022-03-01 18:13:29 -0500

    add speech

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -1,30 +1,26 @@
-const speech = 'Clarke: Today, water and shelter. Tomorrow, food, ok? Picasso, come! Picasso? Please come back! I don’t want to be alone. I don’t want to be alone. Lexa: You’re not.
+const speech = `Clarke: Today, water and shelter. Tomorrow, food, ok? Picasso, come! Picasso? Please come back! I don't want to be alone. I don't want to be alone.
+Lexa: You're not.
 Clarke: What do you want?
 Lexa: To explain.
-Clarke: There’s no need. I get it, I bear it so they don’t have to again.
-Lexa: Such a curious species. You’ve added so much to us already. I’m glad to been wrong about you.
-Clarke: Wait. Does that mean…you are here to take me with you?
+Clarke: There's no need. I get it, I bear it so they don't have to again.
+Lexa: Such a curious species. You've added so much to us already. I'm glad to been wrong about you.
+Clarke: Wait. Does that mean...you are here to take me with you?
 Lexa: No. You can never join us, Clarke. Your actions must have a cost.
 Clarke: Just me? Am I the only human being who has ever sinned?
 Lexa: Of course not, but you are the only test subject from any species anywhere in the universe since the dawn of time who committed murder during a test.
-Clarke: I’d do it again.
+Clarke: I'd do it again.
 Lexa: Madi knew you would say that.
 Clarke: Madi is with you?
-Lexa: In a matter of speaking, yes. Her consciousness has joined ours. She is at peace. She will never feel pain, she will never die. She knew that living here without anyone her own age to love is something you wouldn’t have wanted for her, even if it meant you being alone. I suppose that choice was made easier with the realization you wouldn’t be.
+Lexa: In a matter of speaking, yes. Her consciousness has joined ours. She is at peace. She will never feel pain, she will never die. She knew that living here without anyone her own age to love is something you wouldn't have wanted for her, even if it meant you being alone. I suppose that choice was made easier with the realization you wouldn't be.
 Clarke: Transcendence is a choice? You can choose to come back?
-Lexa: Of course, though until now no one ever had. There won’t be offspring, and they won’t join us when they die. None of them seem to care. A curious species indeed.
-*Clarke runs her friends and hugs Raven, Murphy, Emori, Octavia, Echo, Jackson, Hope, and Niylah*';
+Lexa: Of course, though until now no one ever had. There won't be offspring, and they won't join us when they die. None of them seem to care. A curious species indeed.
+*Clarke runs towards her friends and hugs Raven, Murphy, Emori, Octavia, Echo, Jackson, Hope, and Niylah*`;
 
-// Remove punctuation from the speech. You might have to modify this if your
-// speech contains other punctuation.
-const speechPunctuationRemoved = speech.replace(',', '').replace('.', '');
+const speechPunctuationRemoved = speech.replace(',', '').replace('.', '').replace('?', '').replace(':', '');
 
-// Use a regular expression to split the speech into individual words. You
-// shouldn't need to change this, unless you want to split on characters other
-// than whitespace.
 const wordsArray = speechPunctuationRemoved.split(/\s+/);
 
-// Displays words that have more than 5 characters.
+
 function displayLongWords() {
   const longWordsElement = document.getElementById('long-words');
 
